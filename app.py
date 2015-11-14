79451ac5a37d1e76116702756969fed4f1434a7c
Andrey Kislyuk <akislyuk@dnanexus.com>
2015-11-14 12:17:56 -0800

    WIP

--- app.py
+++ app.py
@@ -6,26 +6,27 @@ import os, sys, json, errno
 from collections import MutableMapping
 
 class Config(MutableMapping):
-    config_home = os.environ.get("XDG_CONFIG_HOME", os.path.expanduser("~/.config"))
+    _config_home = os.environ.get("XDG_CONFIG_HOME", os.path.expanduser("~/.config"))
+
     def __init__(self, name=os.path.basename(__file__), parent=None):
         # TODO: scan XDG_CONFIG_DIRS
-        self.config_dir = os.path.join(self.config_home, name)
-        self.config_file = os.path.join(self.config_dir, "config.json")
+        self._config_dir = os.path.join(self._config_home, name)
+        self._config_file = os.path.join(self._config_dir, "config.json")
         try:
-            with open(self.config_file) as fh:
+            with open(self._config_file) as fh:
                 self._data = json.load(fh)
         except Exception as e:
             self._data = {}
 
     def save(self, mode=0o600):
         try:
-            os.makedirs(self.config_dir)
+            os.makedirs(self._config_dir)
         except OSError as e:
-            if not (e.errno == errno.EEXIST and os.path.isdir(self.config_dir)):
+            if not (e.errno == errno.EEXIST and os.path.isdir(self._config_dir)):
                 raise
-        with open(self.config_file, "w") as fh:
+        with open(self._config_file, "w") as fh:
             json.dump(self._data, fh)
-        os.chmod(self.config_file, mode)
+        os.chmod(self._config_file, mode)
 
     def __getitem__(self, item):
         if item not in self._data:
@@ -34,6 +35,15 @@ class Config(MutableMapping):
 
     def __setitem__(self, key, value):
         self._data[key] = value
+
+    def __getattr__(self, attr):
+        return self.__getitem__(attr)
+
+    def __setattr__(self, attr, value):
+        if attr.startswith("_"):
+            self.__dict__[attr] = value
+        else:
+            self.__setitem__(attr, value)
 
     def __delitem__(self, key):
         del self._data[key]
@@ -46,6 +56,6 @@ class Config(MutableMapping):
         return len(self._data)
 
 config = Config()
-config["test"] = 1
-print(config["test"])
+config.test = 1
+print(config.test)
 config.save()
