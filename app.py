d577cd2a8623186f58ce91258840395197928ecc
Andrey Kislyuk <akislyuk@dnanexus.com>
2015-11-16 06:17:18 +0800

    WIP

--- app.py
+++ app.py
@@ -122,3 +122,14 @@ print(config)
 config.host, config.port = "example.com", 9000
 config.nested_config = {}
 config.nested_config.foo = True
+print(config)
+if "token" not in config:
+    config["token"] = "x"
+
+import argparse
+parser = argparse.ArgumentParser(description=__doc__)
+parser.add_argument("--foo")
+parser.add_argument("--bar")
+args = parser.parse_args()
+config.update(vars(args))
+print(config)
